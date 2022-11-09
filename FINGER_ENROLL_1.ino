bee417311f6d5843d8bfa11f6f5d7e4fc31f514a
Akbar Abdurrahman Jafaruddin <96691090+AkbarAj@users.noreply.github.com>
2022-11-09 19:04:29 +0800

    Update and rename FINGER_ENROLL.ino to FINGER_ENROLL_1.ino

--- FINGER_ENROLL_1.ino
+++ FINGER_ENROLL_1.ino
@@ -1,3 +1,51 @@
+#include <Adafruit_Fingerprint.h>
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial2);
+
+uint8_t id;
+
+void setup()  
+{
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\nAdafruit Fingerprint sensor enrollment");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+}
+
+uint8_t readnumber(void) {
+  uint8_t num = 0;
+  
+  while (num == 0) {
+    while (! Serial.available());
+    num = Serial.parseInt();
+  }
+  return num;
+}
+
+void loop()                     // run over and over again
+{
+  Serial.println("Ready to enroll a fingerprint!");
+  Serial.println("Please type in the ID # (from 1 to 127) you want to save this finger as...");
+  id = readnumber();
+  if (id == 0) {// ID #0 not allowed, try again!
+     return;
+  }
+  Serial.print("Enrolling ID #");
+  Serial.println(id);
+  
+  while (!  getFingerprintEnroll() );
+}
+
 uint8_t getFingerprintEnroll() {
 
   int p = -1;
@@ -135,13 +183,4 @@ uint8_t getFingerprintEnroll() {
     Serial.println("Unknown error");
     return p;
   }   
-}
-uint8_t readnumber(void) {
-  uint8_t num = 0;
-  
-  while (num == 0) {
-    while (! Serial.available());
-    num = Serial.parseInt();
-  }
-  return num;
 }
